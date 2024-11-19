ca3139f74b11561d7b177bfa01b4254cbdf99b0e
anish-saranga <anish.saranga@optimoz.com>
2024-11-19 14:54:20 -0500

    secrets added as props

--- src/ChatWindow/ChatPanel/ChatPanel.jsx
+++ src/ChatWindow/ChatPanel/ChatPanel.jsx
@@ -1,9 +1,57 @@
+import { useEffect, useState } from "react";
+import SECRETS from "../../secrets";
+
+
 function ChatPanel({ messageField }) {
+  const [secretsPresent, setSecretsPresent] = useState(true)
+  // all the convo history
+  const [chatHistory, setChatHistory] = useState([])
+
+
+  // check if secrets are passed as intended
+  useEffect(() => {
+    // if secrets issue, show err
+    if(!SECRETS.BOT_ID || !SECRETS.BOT_SECRET) {
+      setSecretsPresent(false)
+    }
+  }, [])
+
+
+  // set initial message
+  useEffect(() => {
+    setChatHistory([
+      {
+        type: 'bot',
+        message: 'Hello! How can I help you today?'
+      }
+    ])
+  }, [])
+
+
+
   return (
-    <div className="flex-grow">
-    <div>ChatPanel</div>
+  <>
+  {
+    secretsPresent ?
+    // show chat panel
+    <div className="flex-grow overflow-y-scroll px-2 py-1">
+      {
+        chatHistory.forEach((chat, index) => {
+          // render chat bubbles
+        })
+      }
     </div>
-  )
+
+
+    :
+
+    <div className="flex-grow">
+      <h3 className="flex h-full w-full justify-center items-center px-2 text-red-500">Something went wrong! Check with your license provider.</h3>
+    </div>
+
+  }
+  </>
+)
 }
 
 export default ChatPanel
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { r2wc } from 'r2wc';
+// import { r2wc } from 'r2wc';
+import r2wc from "@r2wc/react-to-web-component"
 import App from './App';
 import './index.css'
 
 // Define the web component
-const WebComponent = r2wc(App, React, ReactDOM);
+const WebComponent = r2wc(App, {
+    props:
+    {
+        license: "string",
+    }
+});
 
 // Register the custom element
 customElements.define('botimoz-widget', WebComponent);
