a7ba1c31628b4e7f174de06845edafcced0b9bb9
anish-saranga <anish.saranga@optimoz.com>
2024-11-21 14:06:11 -0500

    speech recognition added

--- src/ChatWindow/ChatInteract/ChatInteract.jsx
+++ src/ChatWindow/ChatInteract/ChatInteract.jsx
@@ -1,4 +1,6 @@
 import { RiMicLine } from "react-icons/ri";
+import { RiMicOffFill } from "react-icons/ri";
+import { useEffect, useState } from "react";
 
 
 function ChatInteract( { setMessageField } ) {
@@ -14,11 +16,58 @@ function ChatInteract( { setMessageField } ) {
     document.getElementById('botimoz-input-field').value = ''
   }
 
+  
+  // speech recognition stuff
+  const [speechRunning, setSpeechRunning] = useState(false)
+
+  useEffect(() => {
+    if(speechRunning) {
+      recognition.start()
+    } else {
+      recognition.stop()
+    }
+    //eslint-disable-next-line
+  }, [speechRunning])
+
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+
+  recognition.onresult = (event) => {
+    // set the message field
+    setMessageField(event.results[0][0].transcript)
+  };
+
+  recognition.onspeechend = () => {
+    recognition.stop();
+    setSpeechRunning(false)
+  };
+  
+  recognition.onerror = (e) => {
+    console.error('Error occurred in recognition: ' + e.error);
+  };
+
+
   return (
     <div className="bg-blue-500 p-1 rounded-b-md">
       <div className="flex justify-start items-center">
-        <div className="text-sm m-1 py-1 px-2 rounded-full hover:bg-blue-400 transition">
-        <RiMicLine size={20} className="text-white"/>
+        <div className="text-sm m-1 py-1 px-2 rounded-full hover:bg-blue-400 transition"
+        onClick={() => setSpeechRunning((prev) => 
+        {
+          if(prev) {
+            recognition.stop()
+          }
+          return !prev
+        }
+        )}
+        >
+          {
+            speechRunning ?
+            <RiMicOffFill size={20} className="text-white"/>
+            : 
+            <RiMicLine size={20} className="text-white"/>
+          }
         </div>
 
         <input id="botimoz-input-field" type="text" placeholder="Type a message..." className="w-full p-1 m-1 rounded-full bg-white text-black" 
